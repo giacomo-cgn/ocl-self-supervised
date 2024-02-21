265d7a6865c5f19be6a56440e39b307160276ce5
Giacomo Cignoni <giacomo.cignoni@gmail.com>
2024-02-21 13:34:36 +0100

    delete old files

--- main_hyperparams.py
+++
@@ -1,8 +0,0 @@
-from src.utils import read_command_line_args
-from search_hyperparams import search_hyperparams
-
-# Main
-# Parse arguments
-args = read_command_line_args()
-# Search hyperparameters
-search_hyperparams(args)
