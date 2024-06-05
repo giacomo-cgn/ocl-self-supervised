3b843d77e33a9870fb39e64ff2029144268e178b
Giacomo Cignoni <giacomo.cignoni@gmail.com>
2024-06-05 21:46:17 +0200

    fixed bug in curriculum probing

--- src/exec_probing.py
+++ src/exec_probing.py
@@ -7,16 +7,16 @@ from .probing_sklearn import ProbingSklearn
 
 
 def exec_probing(kwargs, probing_benchmark, encoder, pretr_exp_idx, probing_tr_ratio_arr, device, probing_upto_pth_dict, probing_separate_pth_dict):
-# Probing on all experiences up to current
-    if kwargs['probing_upto'] and not (kwargs['iid'] or kwargs["random_encoder"]):
-        # Generate upto current exp probing datasets
-        probe_upto_dataset_tr = ConcatDataset([probing_benchmark.train_stream[i] for i in range(pretr_exp_idx+1)])
-        probe_upto_dataset_test = ConcatDataset([probing_benchmark.test_stream[i] for i in range(pretr_exp_idx+1)])
+    # Probing on all experiences
+    if kwargs['probing_upto']:
+        # Generate joint exp probing datasets
+        probe_upto_dataset_tr = ConcatDataset(probing_benchmark.train_stream)
+        probe_upto_dataset_test = ConcatDataset(probing_benchmark.test_stream)
         if kwargs['probing_val_ratio'] > 0:
-            probe_upto_dataset_val = ConcatDataset([probing_benchmark.valid_stream[i] for i in range(pretr_exp_idx+1)])
+            probe_upto_dataset_val = ConcatDataset(probing_benchmark.valid_stream)
 
         for probing_tr_ratio in probing_tr_ratio_arr:
-            probe_save_file = os.path.join(probing_upto_pth_dict[probing_tr_ratio], f'probe_exp_{pretr_exp_idx}.csv')
+            probe_save_file = os.path.join(probing_upto_pth_dict[probing_tr_ratio], f'probe_joint.csv')
 
             probe = ProbingSklearn(encoder, device=device, save_file=probe_save_file,
                                    exp_idx=None, tr_samples_ratio=probing_tr_ratio,
@@ -55,27 +55,3 @@ def exec_probing(kwargs, probing_benchmark, encoder, pretr_exp_idx, probing_tr_r
                     probe.probe(probe_tr_exp_dataset, probe_test_exp_dataset, probe_val_exp_dataset)
                 else:
                     probe.probe(probe_tr_exp_dataset, probe_test_exp_dataset)
-
-    # If iid training, probe upto each experience
-    if kwargs['probing_upto'] and (kwargs['iid'] or kwargs["random_encoder"]):
-        for exp_idx, _ in enumerate(probing_benchmark.train_stream):
-            # Generate upto current exp probing datasets
-            probe_upto_dataset_tr = ConcatDataset([probing_benchmark.train_stream[i] for i in range(exp_idx+1)])
-            probe_upto_dataset_test = ConcatDataset([probing_benchmark.test_stream[i] for i in range(exp_idx+1)])
-            if kwargs['probing_val_ratio'] > 0:
-                probe_upto_dataset_val = ConcatDataset([probing_benchmark.valid_stream[i] for i in range(exp_idx+1)])
-
-            for probing_tr_ratio in probing_tr_ratio_arr:
-                probe_save_file = os.path.join(probing_upto_pth_dict[probing_tr_ratio], f'probe_exp_{exp_idx}.csv')
-
-                probe = ProbingSklearn(encoder, device=device, save_file=probe_save_file,
-                                   exp_idx=None, tr_samples_ratio=probing_tr_ratio,
-                                   mb_size=kwargs["eval_mb_size"], seed=kwargs["seed"],
-                                   probing_type=kwargs["probing_type"], knn_k=kwargs["knn_k"])
-                                            
-                print(f'-- Upto Probing, probe tr ratio: {probing_tr_ratio} --')
-
-                if kwargs['probing_val_ratio'] > 0:
-                    probe.probe(probe_upto_dataset_tr, probe_upto_dataset_test, probe_upto_dataset_val)
-                else:
-                    probe.probe(probe_upto_dataset_tr, probe_upto_dataset_test)
