19f8737aa105d16bede2fb98f62cefbc5916152a
Giacomo Cignoni <giacomo.cignoni@gmail.com>
2025-10-27 19:53:24 +0100

    added correct labels for inaturalist

--- src/inaturalist_dataset.py
+++ src/inaturalist_dataset.py
@@ -23,12 +23,24 @@ def INaturalist(root, download=False, train_transform=None, eval_transform=None,
     #   - 'task_label' (task label for the sample)
 
     # Include only samples with 'include' == 1, split the dataset according to 'task_label'
-    included_samples = train_labels_csv[train_labels_csv['include'] == 1]
-    task_labels = included_samples['task_label'].unique()
+    included_samples_idxs = train_labels_csv['include'] == 1
+    task_labels = train_labels_csv[included_samples_idxs]['task_label'].unique()
     tr_datasets = {}
     for task in task_labels:
-        task_indices = included_samples[included_samples['task_label'] == task].index.tolist()
+        # Indices of samples for this task
+        task_indices = train_labels_csv[included_samples_idxs & (train_labels_csv['task_label'] == task)].index.tolist()
+        # Labels of samples for this task
+        task_labels = train_labels_csv.loc[task_indices, 'target_label'].tolist()
+        # Create subset dataset for this task
         tr_datasets[task] = Subset(dataset_tr, task_indices)
+        # Substitute labels in the subset to be the 'target_label' from the csv
+        if hasattr(tr_datasets[task], 'targets'):
+            tr_datasets[task].targets = task_labels
+        elif hasattr(tr_datasets[task], 'labels'):
+            tr_datasets[task].labels = task_labels
+        else:  
+            raise AttributeError("Subset train dataset has no attribute 'targets' or 'labels', here are all attributes: ", dir(tr_datasets[task]))
+
     # List of task datasets, ordered by task label
     tr_stream = [tr_datasets[task] for task in sorted(task_labels)]
     #  Print how many samples in each task
@@ -43,12 +55,24 @@ def INaturalist(root, download=False, train_transform=None, eval_transform=None,
     test_labels_csv = pd.read_csv(eval_labels_pth)
 
     # Include only samples with 'include' == 1, split the dataset according to 'task_label'
-    included_samples = test_labels_csv[test_labels_csv['include'] == 1]
-    task_labels = included_samples['task_label'].unique()
+    included_samples_idxs = test_labels_csv['include'] == 1
+    task_labels = test_labels_csv[included_samples_idxs]['task_label'].unique()
     test_datasets = {}
     for task in task_labels:
-        task_indices = included_samples[included_samples['task_label'] == task].index.tolist()
+        # Indices of samples for this task
+        task_indices = test_labels_csv[included_samples_idxs & (test_labels_csv['task_label'] == task)].index.tolist()
+        # Labels of samples for this task
+        task_labels = test_labels_csv.loc[task_indices, 'target_label'].tolist()
+        # Create subset dataset for this task
         test_datasets[task] = Subset(dataset_test, task_indices)
+        # Substitute labels in the subset to be the 'target_label' from the csv
+        if hasattr(test_datasets[task], 'targets'):
+            test_datasets[task].targets = task_labels
+        elif hasattr(test_datasets[task], 'labels'):
+            test_datasets[task].labels = task_labels
+        else:  
+            raise AttributeError("Subset test dataset has no attribute 'targets' or 'labels', here are all attributes: ", dir(test_datasets[task]))
+        
     # List of task datasets, ordered by task label
     test_stream = [test_datasets[task] for task in sorted(task_labels)]
 
